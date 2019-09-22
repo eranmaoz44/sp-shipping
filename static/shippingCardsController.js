26f06f3b6ee288ae6e9de288bd6b2bf205e019bb
Eran <e.magnex@gmail.com>
2019-09-22 21:04:21 +0300

    still need to fix order spacinggdamnit

--- static/shippingCardsController.js
+++ static/shippingCardsController.js
@@ -2,18 +2,33 @@ function shippingCardsController($http, $scope){
 
     var self = this
 
-    self.cards = [
-        {
-            'orderNumber' : '10876'
-        }
-    ]
+    self.idLength = 16
+
+    self.cards = []
+
+    self.findIndex = function(id){
+       var res = -1
+       array.forEach(function (item, index) {
+            if(item['id'] == id)
+            res = index
+       });
+
+       return index
+    }
 
     self.addShippingCard = function(){
+        var cardToAdd = {
+                'id' : self.makeID(self.idLength),
+                'orderNumber' : ''
+        }
+
+        self.updateShippingCard(cardToAdd)
+
         self.cards.push(
-            {
-                'orderNumber' : '12312'
-            }
+            cardToAdd
         )
+
+
     }
 
     self.getShippingCards = function(){
@@ -38,7 +53,44 @@ function shippingCardsController($http, $scope){
         });
     }
 
-    self.getShippingCards()
+    self.updateShippingCard = function(card){
+        var cardToUpdate = card
+        var data = cardToUpdate
+
+        var config = {
+            headers : {
+                    'Content-Type': 'application/json;charset=utf-8;'
+            },
+            params : {
+            }
+        }
+
+
+        $http.post('/api/shipping', data, config)
+        .success(function (data, status, headers, config) {
+            $scope.PostDataResponse = data;
+
+        })
+        .error(function (data, status, header, config) {
+            $scope.ResponseDetails = "Data: " + data +
+                "<hr />status: " + status +
+                "<hr />headers: " + header +
+                "<hr />config: " + config;
+
+        });
+    };
+
+    self.makeID = function makeID(length) {
+       var result           = '';
+       var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+       var charactersLength = characters.length;
+       for ( var i = 0; i < length; i++ ) {
+          result += characters.charAt(Math.floor(Math.random() * charactersLength));
+       }
+       return result;
+     }
+
+     self.getShippingCards()
 }
 
 angular
--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 import json
 import os
 import flask
-from flask import Flask, Response
+from flask import Flask, Response, request
 from flask_cors import CORS
 
 application = Flask(__name__)
@@ -18,13 +18,21 @@ def index():
 def get_shipping():
     res = [
         {
+            'id': '1234567891011123',
             'orderNumber': '11111'
         },
         {
+            'id': 'abcdefghijklmnop',
             'orderNumber': '22222'
         }
     ]
     return Response(status=200, response=json.dumps(res))
+
+
+@application.route('/api/shipping', methods=['POST'])
+def set_shipping():
+    print(request.get_json())
+    return Response(status=200)
 
 
 if __name__ == '__main__':
